31892ab27b555ef4abac287823e8ad54757fbec5
AnmoldeepBrar <anmoldeepkaur2910@gmail.com>
2024-03-19 01:19:51 -0500

    Updating about controller

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,5 @@
 class AboutController < ApplicationController
     def index
-        # Fetch details about your data sources as described in 1.1
         @data_sources = [
           { name: 'Books CSV', description: 'CSV file containing book data.' },
           { name: 'Faker Gem', description: 'Gem used to generate fake data for magazines.' }
